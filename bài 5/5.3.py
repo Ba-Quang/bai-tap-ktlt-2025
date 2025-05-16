250dd09b1b7f40a46b6b2f17e60ec42017a41f03
Ba-Quang <baquang339@gmail.com>
2025-05-16 07:12:37 +0700

    Add files via upload

---
+++ bài 5/5.3.py
@@ -0,0 +1,5 @@
+print("Nguyễn Bá Quảng")
+print("MSSV:235752020710045")
+import numpy as np 
+x = np.arange(12, 38) 
+print(x) 
---
+++ bài 5/5.4.py
@@ -0,0 +1,9 @@
+print("Nguyễn Bá Quảng")
+print("MSSV:235752020710045")
+
+import numpy as np
+mang = np.arange(12, 39)
+print("Mảng ban đầu:",mang)
+
+mang_dao_nguoc = np.flip(mang)
+print("Mảng đảo ngược:", mang_dao_nguoc)
