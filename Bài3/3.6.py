c097dc9bc06365bd81da0e9bc46ad3d4ce4a9357
Ba-Quang <baquang339@gmail.com>
2025-05-23 08:12:11 +0700

    Add files via upload

---
+++ Bài3/3.6.py
@@ -0,0 +1,11 @@
+print("NGUYỄN BÁ QUẢNG")
+print("MSSV:235752020710045")
+
+def get_sum(*num): 
+ tmp = 0 
+ # duyet cac tham so 
+ for i in num: 
+  tmp += i 
+ return tmp 
+result = get_sum(1, 2, 3, 4, 5) 
+print(result) 
